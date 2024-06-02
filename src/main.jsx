6f3daeb9be3cd197733a1a6b9762da19c2b14375
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2024-06-03 01:58:13 +0600

    file structure

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,12 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./Routes/publicRouter";
-
-// import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer />
   </React.StrictMode>
 );
-
-{
-  /* <ToastContainer />; */
-}
