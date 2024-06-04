3b62fe8c9c924758ecd08287a29974d14bf02550
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2024-06-04 21:43:58 +0600

    aboutpage done

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,33 +1,232 @@
+import { CiLocationOn, CiMail } from "react-icons/ci";
+import { FaClinicMedical, FaGraduationCap } from "react-icons/fa";
+import { IoMdClock } from "react-icons/io";
+import { MdOutlinePhoneCallback } from "react-icons/md";
+import { PiBuildingOfficeFill } from "react-icons/pi";
+import { TbDentalBroken } from "react-icons/tb";
+
 const About = () => {
   return (
-    <div>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row">
+    <div className="bg-bg-img bg-cover min-h-screen">
+      <h1 className="capitalize lg:py-16 py-5 text-2xl lg:text-7xl font-bold text-blue-500 text-center bg-slate-300">
+        Our honorable doctors
+      </h1>
+      {/* 1st */}
+      <div className="hero min-h-screen border-b-2 border-blue-500">
+        <div className="hero-content flex-col lg:flex-row-reverse">
           <img
-            src="https://i.ibb.co/YW6FqHh/WPic.png"
-            className="lg:max-w-2xl rounded-lg shadow-2xl bg-blue-400"
+            src="https://i.ibb.co/cJ59tKr/2.jpg
+"
+            className="lg:max-w-xl rounded-lg shadow-xl hover:shadow-blue-500 lg:hover:rotate-6 bg-blue-400"
           />
-          <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <ore className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </ore>
-            <button className="btn btn-primary">Get Started</button>
+          <div className="lg:pr-10 text-pretty">
+            <h1 className="lg:text-2xl font-bold uppercase text-blue-700">
+              DR. MD. NAZRUL ISLAM
+            </h1>
+            <h1 className="lg:text-5xl font-bold capitalize text-blue-500">
+              DENTAL & ORAL MAXILLOFACIAL SURGERY
+            </h1>
+            <p className="py-6 text-lg font-medium text-pretty  space-y-3 text-slate-700">
+              <p className="flex gap-2">
+                <FaGraduationCap className="text-blue-600 text-3xl" />
+                BDS, MSc (Singapore)
+              </p>
+              <p className="flex gap-2">
+                <FaClinicMedical className="text-blue-600 text-3xl" />
+                Honourary Medical officer at Shaheed Suhrawardy Medical college
+              </p>
+              <p className="flex gap-2">
+                <TbDentalBroken className="text-blue-600 text-3xl" />
+                Dental Surgeon(Shaheed Suhrawardy Medical college)
+              </p>
+              <p className="flex gap-2">
+                <PiBuildingOfficeFill className="text-blue-600 text-3xl" />
+                Teeth Care (Chamber of Dentistry)
+              </p>
+              <p className="flex gap-2">
+                <IoMdClock className="text-blue-600 text-3xl" />
+                Fri- Mon 03:00 Pm to 10:00 pm
+              </p>
+              <p className="flex gap-2">
+                <CiLocationOn className="text-blue-600 text-3xl" />
+                H-616, -06, Baitu Amam Housing, Adabor, Mohammadpur, Dhaka
+              </p>
+              <p className="flex gap-2">
+                <MdOutlinePhoneCallback className="text-blue-600 text-3xl" />
+                +880 1832 4155
+              </p>
+              <p className="flex gap-2">
+                <CiMail className="text-blue-600 text-3xl" />
+                abdullah.dr@gmail.com
+              </p>
+            </p>
           </div>
         </div>
       </div>
-      <div className="hero min-h-screen bg-base-200">
+      {/* 2nd */}
+      <div className="hero min-h-screen border-b-2 border-blue-500">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img
-            src="https://i.ibb.co/sb2MQWB/ab.png
+            src="https://i.ibb.co/GHHwMwH/3.jpg
 "
-            className="lg:max-w-2xl rounded-lg shadow-2xl bg-blue-400"
+            className="lg:max-w-xl rounded-lg shadow-xl hover:shadow-blue-500 lg:hover:rotate-6 bg-blue-400"
           />
-          <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">
+          <div className="lg:pr-10 text-pretty">
+            <h1 className="lg:text-2xl font-bold uppercase text-blue-700">
+              PROF. DR. MOSTAQUE H SATTAR
+            </h1>
+            <h1 className="lg:text-5xl font-bold capitalize text-blue-500">
+              DENTISTRY
+            </h1>
+            <p className="py-6 text-lg font-medium text-pretty  space-y-3 text-slate-700">
+              <p className="flex gap-2">
+                <FaGraduationCap className="text-blue-600 text-3xl" />
+                BDS, DDS, MCPS, FICCDE, FICD
+              </p>
+              <p className="flex gap-2">
+                <FaClinicMedical className="text-blue-600 text-3xl" />
+                Honourary Medical officer at Shaheed Suhrawardy Medical college
+              </p>
+              <p className="flex gap-2">
+                <TbDentalBroken className="text-blue-600 text-3xl" />
+                Dental Surgeon(Shaheed Suhrawardy Medical college)
+              </p>
+              <p className="flex gap-2">
+                <PiBuildingOfficeFill className="text-blue-600 text-3xl" />
+                Teeth Care (Chamber of Dentistry)
+              </p>
+              <p className="flex gap-2">
+                <IoMdClock className="text-blue-600 text-3xl" />
+                Fri- Mon 03:00 Pm to 10:00 pm
+              </p>
+              <p className="flex gap-2">
+                <CiLocationOn className="text-blue-600 text-3xl" />
+                H-616, -06, Baitu Amam Housing, Adabor, Mohammadpur, Dhaka
+              </p>
+              <p className="flex gap-2">
+                <MdOutlinePhoneCallback className="text-blue-600 text-3xl" />
+                +880 1832 4155
+              </p>
+              <p className="flex gap-2">
+                <CiMail className="text-blue-600 text-3xl" />
+                abdullah.dr@gmail.com
+              </p>
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* 3rd */}
+      <div className="hero min-h-screen border-b-2 border-blue-500">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src="https://i.ibb.co/gJGxMvx/4.jpg
+"
+            className="lg:max-w-xl rounded-lg shadow-xl hover:shadow-blue-500 lg:hover:rotate-6 bg-blue-400"
+          />
+          <div className="lg:pr-10 text-pretty">
+            <h1 className="lg:text-2xl font-bold uppercase text-blue-700">
+              DR. SPINA LUNA BISWAS
+            </h1>
+            <h1 className="lg:text-5xl font-bold capitalize text-blue-500">
+              DENTAL & ORAL MAXILLOFACIAL SURGERY
+            </h1>
+            <p className="py-6 text-lg font-medium text-pretty  space-y-3 text-slate-700">
+              <p className="flex gap-2">
+                <FaGraduationCap className="text-blue-600 text-3xl" />
+                BDS, FCPS (Oral & Maxillofacial Surgery)
+              </p>
+              <p className="flex gap-2">
+                <FaClinicMedical className="text-blue-600 text-3xl" />
+                Honourary Medical officer at Shaheed Suhrawardy Medical college
+              </p>
+              <p className="flex gap-2">
+                <TbDentalBroken className="text-blue-600 text-3xl" />
+                Dental Surgeon(Shaheed Suhrawardy Medical college)
+              </p>
+              <p className="flex gap-2">
+                <PiBuildingOfficeFill className="text-blue-600 text-3xl" />
+                Teeth Care (Chamber of Dentistry)
+              </p>
+              <p className="flex gap-2">
+                <IoMdClock className="text-blue-600 text-3xl" />
+                Fri- Mon 03:00 Pm to 10:00 pm
+              </p>
+              <p className="flex gap-2">
+                <CiLocationOn className="text-blue-600 text-3xl" />
+                H-616, -06, Baitu Amam Housing, Adabor, Mohammadpur, Dhaka
+              </p>
+              <p className="flex gap-2">
+                <MdOutlinePhoneCallback className="text-blue-600 text-3xl" />
+                +880 1832 4155
+              </p>
+              <p className="flex gap-2">
+                <CiMail className="text-blue-600 text-3xl" />
+                abdullah.dr@gmail.com
+              </p>
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* 4th */}
+      <div className="hero min-h-screen border-b-2 border-blue-500">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src="https://i.ibb.co/LzH8XPJ/1.jpg"
+            className="lg:max-w-xl rounded-lg shadow-xl hover:shadow-blue-500 lg:hover:rotate-6 bg-blue-400"
+          />
+          <div className="lg:pr-10 text-pretty">
+            <h1 className="lg:text-2xl font-bold uppercase text-blue-700">
+              DR. ASHIS KUMAR BISWAS
+            </h1>
+            <h1 className="lg:text-5xl font-bold capitalize text-blue-500">
+              DENTAL SURGERY, ORTHODONTIC & DENTOFACIAL ORTHOPAEDICS
+            </h1>
+            <p className="py-6 text-lg font-medium text-pretty  space-y-3 text-slate-700">
+              <p className="flex gap-2">
+                <FaGraduationCap className="text-blue-600 text-3xl" />
+                BDS, MCPS (Dental Surgery), FCPS (Orthodontic & Dentofacial
+                Orthopaedics)
+              </p>
+              <p className="flex gap-2">
+                <FaClinicMedical className="text-blue-600 text-3xl" />
+                Honourary Medical officer at Shaheed Suhrawardy Medical college
+              </p>
+              <p className="flex gap-2">
+                <TbDentalBroken className="text-blue-600 text-3xl" />
+                Dental Surgeon(Shaheed Suhrawardy Medical college)
+              </p>
+              <p className="flex gap-2">
+                <PiBuildingOfficeFill className="text-blue-600 text-3xl" />
+                Teeth Care (Chamber of Dentistry)
+              </p>
+              <p className="flex gap-2">
+                <IoMdClock className="text-blue-600 text-3xl" />
+                Fri- Mon 03:00 Pm to 10:00 pm
+              </p>
+              <p className="flex gap-2">
+                <CiLocationOn className="text-blue-600 text-3xl" />
+                H-616, -06, Baitu Amam Housing, Adabor, Mohammadpur, Dhaka
+              </p>
+              <p className="flex gap-2">
+                <MdOutlinePhoneCallback className="text-blue-600 text-3xl" />
+                +880 1832 4155
+              </p>
+              <p className="flex gap-2">
+                <CiMail className="text-blue-600 text-3xl" />
+                abdullah.dr@gmail.com
+              </p>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Mission */}
+      <div className="hero min-h-screen bg-bg-img bg-cover">
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Our Mission</h1>
+            <p className="mb-5">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
               et a id nisi.
