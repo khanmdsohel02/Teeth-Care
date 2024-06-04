828e5a678ebe6419c670ff3ac2e302cad4094d3e
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2024-06-04 19:55:30 +0600

    about ui done

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,43 @@
-import React from "react";
-
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <img
+            src="https://i.ibb.co/YW6FqHh/WPic.png"
+            className="lg:max-w-2xl rounded-lg shadow-2xl bg-blue-400"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <ore className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </ore>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src="https://i.ibb.co/sb2MQWB/ab.png
+"
+            className="lg:max-w-2xl rounded-lg shadow-2xl bg-blue-400"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
