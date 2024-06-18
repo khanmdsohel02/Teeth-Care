ac6ab001597d3a8ebb5406d517cdbf48ca38ca3f
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2024-06-18 23:23:16 +0600

    404 page create

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="flex justify-center items-center flex-col bg-blue-100 h-[100vh]">
+    <div className="flex justify-center items-center flex-col bg-red-100 h-[100vh]">
       <img
         className="w-[95%] lg:w-[50%] rounded-lg mb-6"
-        src="https://i.ibb.co/mzsvDnD/notfound.jpg"
+        src="https://i.ibb.co/FsCwPfJ/404.jpg"
         alt="404"
       />
       <Link to="/">
